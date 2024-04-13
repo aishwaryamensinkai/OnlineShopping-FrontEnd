07921a4e785fb1850bf679346b7d18b89ba12660
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-04-13 00:00:16 -0400

    updated to 14

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
@@ -11,18 +11,18 @@ export class ProfileComponent implements OnInit {
   user: any;
   userId: any;
   message: String = "";
-  private _userform!: FormGroup;
+  private _userform!: UntypedFormGroup;
 
-  public get userform(): FormGroup {
+  public get userform(): UntypedFormGroup {
     return this._userform;
   }
-  public set userform(value: FormGroup) {
+  public set userform(value: UntypedFormGroup) {
     this._userform = value;
   }
   isVisible: boolean = false;
 
 
-  constructor(private formBuilder: FormBuilder, private registrationService: RegistrationService) { }
+  constructor(private formBuilder: UntypedFormBuilder, private registrationService: RegistrationService) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -36,10 +36,10 @@ export class ProfileComponent implements OnInit {
     //   userName: new FormControl([this.user ? this.user.userName : ''])
     // })
     this.userform = this.formBuilder.group({
-      name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z\\s]*$')]),
-      emailId: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
-      userName: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z]).{5,}')])
+      name: new UntypedFormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z\\s]*$')]),
+      emailId: new UntypedFormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      userName: new UntypedFormControl('', [Validators.required]),
+      password: new UntypedFormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z]).{5,}')])
     })
 
   }
